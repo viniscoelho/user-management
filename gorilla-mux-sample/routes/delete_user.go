1ab4abc31090d0dd94db5bb091dea7c2db4eff8d
Vinicius Coelho <vinicius.coelho@sap.com>
2019-11-27 22:23:26 -0300

    Implement DeleteUser API

--- gorilla-mux-sample/routes/delete_user.go
+++ gorilla-mux-sample/routes/delete_user.go
@@ -1,8 +1,11 @@
 package routes
 
 import (
+	"log"
 	"net/http"
 	users "user-management/mockgen-sample"
+
+	"github.com/gorilla/mux"
 )
 
 type DeleteUserHandler struct {
@@ -10,5 +13,23 @@ type DeleteUserHandler struct {
 }
 
 func (h DeleteUserHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	auth := r.Header.Get("Authorization")
+	if auth != authorizationHeaderToken {
+		log.Printf("Unauthorized request to resource: missing authorization header")
+		rw.WriteHeader(http.StatusUnauthorized)
+		rw.Write([]byte("unauthorized"))
+		return
+	}
 
+	vars := mux.Vars(r)
+	username := vars[resourceName]
+	err := h.um.DeleteUser(username)
+	if err != nil {
+		log.Printf("Error: %s", err)
+		rw.WriteHeader(http.StatusInternalServerError)
+		rw.Write([]byte("internal server error"))
+		return
+	}
+
+	rw.WriteHeader(http.StatusNoContent)
 }
