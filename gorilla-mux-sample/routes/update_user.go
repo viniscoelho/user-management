468946fcaca2e9b971912d03d67da1a7cd696869
Vinicius Coelho <vinicius.coelho@sap.com>
2019-11-27 22:36:40 -0300

    Implement UpdateUser API

--- gorilla-mux-sample/routes/update_user.go
+++ gorilla-mux-sample/routes/update_user.go
@@ -1,6 +1,9 @@
 package routes
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
 	"net/http"
 	users "user-management/mockgen-sample"
 )
@@ -10,5 +13,60 @@ type UpdateUserHandler struct {
 }
 
 func (h UpdateUserHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	auth := r.Header.Get("Authorization")
+	if auth != authorizationHeaderToken {
+		log.Printf("Unauthorized request to resource: missing authorization header")
+		rw.WriteHeader(http.StatusUnauthorized)
+		rw.Write([]byte("unauthorized"))
+		return
+	}
 
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("Error: %s", err)
+		rw.WriteHeader(http.StatusInternalServerError)
+		rw.Write([]byte("internal server error"))
+		return
+	}
+
+	newUserDTO := users.UserDTO{}
+	err = json.Unmarshal(body, &newUserDTO)
+	if err != nil {
+		log.Printf("Error: %s", err)
+		rw.WriteHeader(http.StatusInternalServerError)
+		rw.Write([]byte("internal server error"))
+		return
+	}
+
+	newUser, err := users.NewUserFromDTO(newUserDTO)
+	if err != nil {
+		log.Printf("Error: %s", err)
+
+		switch err.(type) {
+		case users.InvalidPasswordError:
+			rw.WriteHeader(http.StatusBadRequest)
+			rw.Write([]byte("invalid password: it should contain at least 8 characters"))
+		default:
+			rw.WriteHeader(http.StatusInternalServerError)
+			rw.Write([]byte("internal server error"))
+		}
+
+		return
+	}
+
+	err = h.um.UpdateUser(newUser)
+	if err != nil {
+		log.Printf("Error: %s", err)
+
+		switch err.(type) {
+		case users.UserDoesNotExistError:
+			rw.WriteHeader(http.StatusNotFound)
+			rw.Write([]byte("username does not exist"))
+		default:
+			rw.WriteHeader(http.StatusInternalServerError)
+			rw.Write([]byte("internal server error"))
+		}
+
+		return
+	}
 }
